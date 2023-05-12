ec2f9397d899efd83f9a64b2e565035f4f99c096
Logan King <65763483+loganator956@users.noreply.github.com>
2023-05-12 14:56:49 +0100

    Create the basic form layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.applicationsTabPage = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.availableApplicationsCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.installAllToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.appInformationTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.applicationsLogTextBox = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
+            this.applicationsTabPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -55,8 +79,7 @@
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.applicationsTabPage);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 24);
             this.tabControl1.Name = "tabControl1";
@@ -64,25 +87,139 @@
             this.tabControl1.Size = new System.Drawing.Size(800, 426);
             this.tabControl1.TabIndex = 1;
             // 
-            // tabPage1
+            // applicationsTabPage
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 24);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 398);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.applicationsTabPage.Controls.Add(this.splitContainer1);
+            this.applicationsTabPage.Controls.Add(this.statusStrip1);
+            this.applicationsTabPage.Location = new System.Drawing.Point(4, 24);
+            this.applicationsTabPage.Name = "applicationsTabPage";
+            this.applicationsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.applicationsTabPage.Size = new System.Drawing.Size(792, 398);
+            this.applicationsTabPage.TabIndex = 1;
+            this.applicationsTabPage.Text = "Applications";
+            this.applicationsTabPage.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // splitContainer1
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 24);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 72);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(786, 370);
+            this.splitContainer1.SplitterDistance = 262;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.availableApplicationsCheckedListBox);
+            this.groupBox1.Controls.Add(this.toolStrip1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 370);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Available Applications";
+            // 
+            // availableApplicationsCheckedListBox
+            // 
+            this.availableApplicationsCheckedListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.availableApplicationsCheckedListBox.FormattingEnabled = true;
+            this.availableApplicationsCheckedListBox.Location = new System.Drawing.Point(3, 44);
+            this.availableApplicationsCheckedListBox.Name = "availableApplicationsCheckedListBox";
+            this.availableApplicationsCheckedListBox.Size = new System.Drawing.Size(256, 323);
+            this.availableApplicationsCheckedListBox.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.installAllToolStripButton});
+            this.toolStrip1.Location = new System.Drawing.Point(3, 19);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(256, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // installAllToolStripButton
+            // 
+            this.installAllToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.installAllToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("installAllToolStripButton.Image")));
+            this.installAllToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.installAllToolStripButton.Name = "installAllToolStripButton";
+            this.installAllToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.installAllToolStripButton.Text = "Install All";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(3, 373);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(786, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.groupBox3);
+            this.splitContainer2.Size = new System.Drawing.Size(520, 370);
+            this.splitContainer2.SplitterDistance = 250;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // appInformationTextBox
+            // 
+            this.appInformationTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.appInformationTextBox.Location = new System.Drawing.Point(3, 19);
+            this.appInformationTextBox.Multiline = true;
+            this.appInformationTextBox.Name = "appInformationTextBox";
+            this.appInformationTextBox.Size = new System.Drawing.Size(244, 348);
+            this.appInformationTextBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.appInformationTextBox);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(250, 370);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Application Information";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.applicationsLogTextBox);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(266, 370);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Applications Log";
+            // 
+            // applicationsLogTextBox
+            // 
+            this.applicationsLogTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.applicationsLogTextBox.Location = new System.Drawing.Point(3, 19);
+            this.applicationsLogTextBox.Multiline = true;
+            this.applicationsLogTextBox.Name = "applicationsLogTextBox";
+            this.applicationsLogTextBox.Size = new System.Drawing.Size(260, 348);
+            this.applicationsLogTextBox.TabIndex = 1;
             // 
             // Form1
             // 
@@ -97,6 +234,24 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
+            this.applicationsTabPage.ResumeLayout(false);
+            this.applicationsTabPage.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,7 +262,17 @@
         private MenuStrip menuStrip1;
         private ToolStripMenuItem fileToolStripMenuItem;
         private TabControl tabControl1;
-        private TabPage tabPage1;
-        private TabPage tabPage2;
+        private TabPage applicationsTabPage;
+        private SplitContainer splitContainer1;
+        private GroupBox groupBox1;
+        private CheckedListBox availableApplicationsCheckedListBox;
+        private ToolStrip toolStrip1;
+        private ToolStripButton installAllToolStripButton;
+        private StatusStrip statusStrip1;
+        private SplitContainer splitContainer2;
+        private GroupBox groupBox2;
+        private TextBox appInformationTextBox;
+        private GroupBox groupBox3;
+        private TextBox applicationsLogTextBox;
     }
 }
