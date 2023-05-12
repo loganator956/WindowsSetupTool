a4843e6987ef4d2250a5c376b4bba14f0962f1b3
Logan King <65763483+loganator956@users.noreply.github.com>
2023-05-12 16:01:50 +0100

    implement basic installing through winget

---
+++ ApplicationSource.cs
@@ -0,0 +1,21 @@
+﻿namespace WindowsSetupTool
+{
+    struct ApplicationSource
+    {
+        public ApplicationSource()
+        {
+            AppName = "";
+            AppDescription = "";
+            AppID = "";
+        }
+        public string AppName;
+        public string AppDescription;
+        public string AppID;
+        // TODO: Implement dependencies (Could just add multiple AppIDs?)
+
+        public override string ToString()
+        {
+            return AppName;
+        }
+    }
+}
