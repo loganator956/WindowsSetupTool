be6c9113f7eeb1deb2777b0aced27b0c34520fb1
Logan King <65763483+loganator956@users.noreply.github.com>
2023-05-12 16:22:17 +0100

    Remove descriptions

--- ApplicationSource.cs
+++ ApplicationSource.cs
@@ -5,11 +5,9 @@
         public ApplicationSource()
         {
             AppName = "";
-            AppDescription = "";
             AppIDs = new string[0];
         }
         public string AppName;
-        public string AppDescription;
         public string[] AppIDs;
 
         public override string ToString()
