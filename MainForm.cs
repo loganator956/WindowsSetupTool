9d38748a37777c476ef895f871d7b888c17117ea
Logan King <65763483+loganator956@users.noreply.github.com>
2023-05-24 14:55:11 +0100

    remove unnecessary usings

--- MainForm.cs
+++ MainForm.cs
@@ -1,8 +1,5 @@
 using Newtonsoft.Json;
 using System.ComponentModel;
-using System.Diagnostics;
-using System.Net;
-using System.Security.Policy;
 using WindowsSetupTool.Installers;
 
 namespace WindowsSetupTool
